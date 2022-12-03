3c5707d7257b71b1ee7bec789146a558824bba1a
bassem eloraby <BASEM12683@GMAIL.COM>
2022-12-03 13:18:48 +0300

    filtration done

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors');
 const connectDB = require('./config/db');
+const cors = require('cors')
+
 connectDB();
 const app = express()
-
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use('/tasks', require('./routes/taskRaoutes'));
---
+++ readme.md
@@ -0,0 +1,1 @@
+connect back to the front
